ae5c871ee92f3798b84f9f463d4154cf6a8c2107
begmuhommet jumayev <begmuhommet.jumayev@gmail.com>
2023-03-27 20:26:36 +0800

    :bug: Fix bug with Porsche model

--- src/components/models/Porsche.tsx
+++ src/components/models/Porsche.tsx
@@ -7,76 +7,37 @@ const Porsche = (props: any) => {
 
   return (
     <group {...props} dispose={null}>
-      <group
-        position={[0, -0.1, 0]}
-        rotation={[-Math.PI / 2, 0, 0]}
-        scale={0.81}
-      >
-        <group rotation={[Math.PI / 2, 0, 0]}>
-          <group position={[0, 0.68, -2.62]} scale={[1, 0.96, 1]}>
-            <mesh
-              geometry={nodes.mesh_1_instance_0.geometry}
-              material={materials["930_plastics"]}
-              position={[-7.97, -0.1, -7.97]}
-              scale={0}
-            />
-          </group>
-          <group
-            position={[0, 0.47, 3.12]}
-            rotation={[-2.83, 0, Math.PI]}
-            scale={[1, 0.96, 1]}
-          >
-            <mesh
-              geometry={nodes.mesh_1_instance_1.geometry}
-              material={materials["930_plastics"]}
-              position={[-7.97, -0.1, -7.97]}
-              scale={0}
-            />
-          </group>
-        </group>
-      </group>
-      <group position={[-7.97, -0.1, -7.97]} scale={0}>
-        <mesh geometry={nodes.mesh_0.geometry} material={materials.paint} />
+      <group scale={1}>
+        <mesh geometry={nodes.Mesh_0_1.geometry} material={materials.paint} />
         <mesh
-          geometry={nodes.mesh_0_1.geometry}
+          geometry={nodes.Mesh_0_2.geometry}
           material={materials["930_chromes"]}
         />
-        <mesh geometry={nodes.mesh_0_2.geometry} material={materials.black} />
+        <mesh geometry={nodes.Mesh_0_3.geometry} material={materials.black} />
         <mesh
-          geometry={nodes.mesh_0_3.geometry}
+          geometry={nodes.Mesh_0_4.geometry}
           material={materials["930_lights"]}
         />
-        <mesh geometry={nodes.mesh_0_4.geometry} material={materials.glass} />
+        <mesh geometry={nodes.Mesh_0_5.geometry} material={materials.glass} />
         <mesh
-          geometry={nodes.mesh_0_5.geometry}
+          geometry={nodes.Mesh_0_6.geometry}
           material={materials["930_stickers"]}
         />
         <mesh
-          geometry={nodes.mesh_0_6.geometry}
+          geometry={nodes.Mesh_0_7.geometry}
           material={materials["930_plastics"]}
         />
         <mesh
-          geometry={nodes.mesh_0_7.geometry}
+          geometry={nodes.Mesh_0_8.geometry}
           material={materials["930_lights_refraction"]}
         />
         <mesh
-          geometry={nodes.mesh_0_8.geometry}
+          geometry={nodes.Mesh_0_9.geometry}
           material={materials["930_rim"]}
         />
         <mesh
-          geometry={nodes.mesh_0_9.geometry}
+          geometry={nodes.Mesh_0_10.geometry}
           material={materials["930_tire"]}
-        />
-      </group>
-      <group position={[-7.97, -0.1, -7.97]} scale={0}>
-        <mesh geometry={nodes.mesh_2.geometry} material={materials.plate} />
-        <mesh
-          geometry={nodes.mesh_2_1.geometry}
-          material={materials.DefaultMaterial}
-        />
-        <mesh
-          geometry={nodes.mesh_2_2.geometry}
-          material={materials["Material.001"]}
         />
       </group>
     </group>
